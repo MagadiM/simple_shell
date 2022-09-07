31a3e31131a9ed401adbce4533a05087e2212804
MagadiM <milliemagadi4@gmail.com>
2022-09-07 07:45:08 -0700

    command line interpreter

--- shell.h
+++ shell.h
@@ -1,23 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h> /*header defines fork function*/
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <time.h> 
-#include <stdbool.h> /*header contains four macros for the boolean data type*/
+void print_prompt1(void);
+void print_prompt2(void);
 
-void print_env(void);
+char *read_cmd(void);
+#endif
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
-
-
-#endif /*SHELL_H*/
